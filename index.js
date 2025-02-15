cb0d9595f605aae881a60f418429a6f1eafccc99
Akash_Lakabshetti <akashlakabshetti@gmail.com>
2025-02-15 15:14:41 +0530

    Add

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const name = Symbol('name');
 const obj = {
-    [name] : 'Akash Lakabshetti',
+    name : 'Akash',
+    age : 52
+};
+for (const key in obj) {
+   console.log(key,obj[key]);
+   
 }
-console.log(obj[name]);
